4461c16ecc5cae11186ed749ba127b9e9b40ec38
samuelmalik <samuelmalik9@gmail.com>
2022-09-24 16:41:33 +0200

    added function returning array of 4 characters long strings

--- Open-Lab-04.08/StringTools.cs
+++ Open-Lab-04.08/StringTools.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Open_Lab_04._08
 {
@@ -6,7 +7,26 @@ namespace Open_Lab_04._08
     {
         public string[] IsFourLetters(string[] strings)
         {
-            throw new NotImplementedException();
+            int loopsNumber = 0;
+            int x = 0;
+            for (int i = 0; i < strings.Length; i++)
+            {
+                if (strings[i].Length == 4)
+                {
+                    loopsNumber++;
+                }
+            }
+            string[] newArray = new string[loopsNumber];
+
+            for (int i = 0; i < strings.Length; i++)
+            {
+                if (strings[i].Length == 4)
+                {
+                    newArray[x] = strings[i];
+                    x++;
+                }
+            }
+            return newArray;
         }
     }
 }
